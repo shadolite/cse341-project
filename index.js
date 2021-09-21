edf9c5d4dbec5cf8329d178686256fe584b5676c
shadolite <amy.kawaii@gmail.com>
2021-09-20 23:06:58 -0600

    ta02 base activity complete

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 
 // Route setup. You can implement more in the future!
 const ta01Routes = require('./routes/ta01');
-const ta02Routes = require('./routes/ta02');
+const ta02Data = require('./routes/ta02');
 const ta03Routes = require('./routes/ta03');
 const ta04Routes = require('./routes/ta04');
 
@@ -33,9 +33,9 @@ app
   // For view engine as hbs (Handlebars)
   //.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'})) // For handlebars
   //.set('view engine', 'hbs')
-  .use(bodyParser({ extended: false })) // For parsing the body of a POST
+  .use(bodyParser.urlencoded({ extended: false })) // For parsing the body of a POST
   .use('/ta01', ta01Routes)
-  .use('/ta02', ta02Routes)
+  .use('/ta02', ta02Data.routes)
   .use('/ta03', ta03Routes)
   .use('/ta04', ta04Routes)
   .get('/', (req, res, next) => {
--- routes/ta02.js
+++ routes/ta02.js
@@ -1,15 +1,39 @@
-//TA02 PLACEHOLDER
-// Remember, you can make more of these placeholders yourself!
 const express = require('express');
 const router = express.Router();
+const users = [];
 
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
     path: '/ta02', // For pug, EJS
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
+    users: users,
   });
 });
 
-module.exports = router;
+router.post('/addUser', (req, res, next) => {
+  users.push({name: req.body.addUsername});
+  res.redirect('/ta02');
+});
+
+router.post('/removeUser', (req, res, next) => {
+  let name = {name: req.body.removeUsername};
+  var nameIndex = -1;
+
+  for (let index = 0; index < users.length; index++) {
+    if (users[index].name === name.name){
+      nameIndex = index;
+      break;
+    }
+  }
+
+  if (nameIndex > -1) {
+    users.splice(nameIndex, 1);
+  }
+
+  res.redirect('/ta02');
+});
+
+exports.routes = router;
+exports.users = users;
